2a2c6e0066a37095336a02834702b7732dcc593a
LeeDongHoon <likelion.leedh@gmail.com>
2025-12-15 23:06:07 +0900

    [test] 테스트용 설정파일 생성

--- src/test/java/com/example/reservation_solution/ReservationSolutionApplicationTests.java
+++ src/test/java/com/example/reservation_solution/ReservationSolutionApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.reservation_solution;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ReservationSolutionApplicationTests {
 
 	@Test
